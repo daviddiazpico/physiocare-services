ea35faf1ef752a74d89dddcf2c7d8779104ada5d
daviddiazpico <daviddiazpico@gmail.com>
2025-05-22 21:13:46 +0200

    Añadido politica CORS

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import * as express from 'express';
 import { AppModule } from './app.module';
+import * as cors from 'cors';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.use(express.json({ limit: "25mb" }))
+  app.use(
+    cors({
+      origin: '*',
+      credentials: true,
+    }),
+  );
+  app.use(express.json({ limit: '25mb' }));
   app.use('/images', express.static(__dirname + '/../images'));
   await app.listen(process.env.PORT ?? 3000);
 }
